d5334e47bb659868b0c0371a04d146d88f6b28b9
Daniel Puentes Camacho <daniel.puentes.ca@tutanota.com>
2021-01-31 13:47:40 -0600

    implementing pseudocode 2

--- b-iri.sol
+++ b-iri.sol
@@ -37,9 +37,53 @@ contract TimeLockedDeposit{
             Set = _Set;
             A_i = _A_i;
         }
+        
+        function driver_deposit(uint256 memory _driver_deposit) public view {
+            require(block.timestamp < expiration);
+            require(msg.sender == driver);
+            require(msg.value == DriverDeposit);
+            require(now < t_deadline)
+            Balance = Balance + _driver_deposit;
+        }
+
+        function Proof_Of_Arrival(string memory _pi, string memory _omega) public {
+            require(msg.sender == driver);
+            require(now == t_0_r);
+            if(ZKMS.Verifier(_pi)){
+                b_iri.pick_up_rep[msg.sender] = b_iri.pick_up_rep[msg.sender] + 1;
+                transfer(balance,driver);
+            }
+        }
+        
+        function Fine_driver() public {
+            require(block.timestamp > expiration);
+            require(msg.sender == rider);
+            transfer(balance,rider);
+        }
+    
 }
 
 //////////////////
 contract PaymentContract{
-        string public hello = "Hello PaymentContract!";
+    address payable rider; // Rider address
+    address payable driver; // driver address 
+
+    uint public TripDist;
+    
+    constructor(address _driver,uint _TripDist, string _t_d_r) {
+        driver = _driver;
+        TripDist = _TripDist;
+    }
+    
+    function Proof_of_distance(uint memory _elapsed_distance) public {
+        require(msg.sender == rider)
+        while(TripDist <= _elapsed_distance){
+            transfer(balance * _elapsed_distance, pk_d)
+            TripDist = TripDist - _elapsed_distance;
+            if(TripDist == 0){
+                b-iri.completed_trip_rep[driver] = b-iri.completed_trip_rep[driver] + 1;    
+            }
+        }
+    }
+}
 }
